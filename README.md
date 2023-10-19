144591e3fafc549c10ea4794ea99067440b0eb0c
GBr1 <giovannididio.bruno@gmail.com>
2023-10-19 12:13:18 +0200

    yo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dirty_cam
 A quick and dirty cam for ROS2
+
+It works only on RoadRunner rover.
+
+
+```bash
+ros2 run dirty_cam dirty_cam_node
+```
+
+output topic: `/dirty_cam/image_raw`
+
+> Copyright © 2023 G. Bruno - gbr1.github.io
--- dirty_cam/dirty_cam_node.py
+++ dirty_cam/dirty_cam_node.py
@@ -8,38 +8,26 @@ from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
 
-class MinimalPublisher(Node):
-
+class CameraPublisher(Node):
     def __init__(self):
         super().__init__('dirty_cam')
-        self.camera
-        self.image_publisher = self.create_publisher(Image, '/dirty_cam/image_raw', 10)
-        timer_period = 0.001  # seconds
+        #self.camera = cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture('v4l2src device=/dev/video3 ! video/x-raw, framerate=30/1, width=640, height=480 ! appsink', cv2.CAP_GSTREAMER)
+        self.cv_bridge = CvBridge()
+        self.image_publisher = self.create_publisher(Image, '/dirty_cam/image_raw', 1)
+        timer_period = 0.001
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
-        msg = Image()
-        time_now = self.get_clock().now().to_msg()
-        msg.header.frame_id='base_link'
-        msg.header.stamp=time_now
-        msg.height=480
-        msg.width=640
-        msg.data= 
-
-        self.image_publisher.publish(msg)
+        ret, capture = self.camera.read()
+        self.image_publisher.publish(self.cv_bridge.cv2_to_imgmsg(capture,"bgr8"))
 
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    camera_publisher = CameraPublisher()
+    rclpy.spin(camera_publisher)
+    camera_publisher.destroy_node()
     rclpy.shutdown()
 
 
